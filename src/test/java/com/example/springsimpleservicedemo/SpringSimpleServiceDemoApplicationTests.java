45786fd1b2968718badc7a335e271c3f27fb3357
pavlobarabah <pavlobarabah@gmail.com>
2024-10-29 15:39:51 +0200

    google cloud

---
+++ src/test/java/com/example/springsimpleservicedemo/SpringSimpleServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsimpleservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleServiceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
